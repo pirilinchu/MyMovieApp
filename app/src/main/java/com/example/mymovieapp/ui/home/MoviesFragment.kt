3ff85ba7262f191151e746422e8f261b38501078
Santiago Mendoza Barriga <Santiago.Mendoza@jalasoft.com>
2021-03-02 18:13:15 -0400

    Refactor MoviesFragment in order to implement MVVM

--- app/src/main/java/com/example/mymovieapp/ui/home/MoviesFragment.kt
+++ app/src/main/java/com/example/mymovieapp/ui/home/MoviesFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymovieapp.R
@@ -23,132 +25,47 @@ import retrofit2.Response
 
 class MoviesFragment : Fragment() {
 
-    private lateinit var recyclerViewNow: RecyclerView
-    private lateinit var recyclerViewPopular: RecyclerView
-    private lateinit var recyclerViewIncoming: RecyclerView
-
-    private lateinit var moviesNow: List<Movie>
-    private lateinit var moviesPopular: List<Movie>
-    private lateinit var moviesIncoming: List<Movie>
+    private lateinit var viewModel: MoviesViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        var root: View = inflater.inflate(R.layout.fragment_movies, container, false)
-        recyclerViewNow = root.findViewById(R.id.recyclerViewNow)
-        recyclerViewPopular = root.findViewById(R.id.recyclerViewPopular)
-        recyclerViewIncoming = root.findViewById(R.id.recyclerViewIncoming)
+        viewModel = ViewModelProvider(this).get(MoviesViewModel::class.java)
 
-        var recyclerViewAdapterNow: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesNow)
-        var recyclerViewAdapterPopular: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesPopular)
-        var recyclerViewAdapterIncoming: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesIncoming)
+        loadMovies()
 
-        recyclerViewNow.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewNow.adapter = recyclerViewAdapterNow
+        val root: View = inflater.inflate(R.layout.fragment_movies, container, false)
 
-        recyclerViewPopular.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewPopular.adapter = recyclerViewAdapterPopular
+        val recyclerViewNow: RecyclerView = root.findViewById(R.id.recyclerViewNow)
+        val recyclerViewPopular: RecyclerView = root.findViewById(R.id.recyclerViewPopular)
+        val recyclerViewIncoming: RecyclerView = root.findViewById(R.id.recyclerViewIncoming)
 
-        recyclerViewIncoming.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewIncoming.adapter = recyclerViewAdapterIncoming
+        viewModel.moviesNow.observe(viewLifecycleOwner, Observer {
+            var recyclerViewAdapterNow: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), it)
+            recyclerViewNow.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewNow.adapter = recyclerViewAdapterNow
+        })
 
+        viewModel.moviesIncoming.observe(viewLifecycleOwner, Observer {
+            var recyclerViewAdapterIncoming: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), it)
+            recyclerViewIncoming.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewIncoming.adapter = recyclerViewAdapterIncoming
+        })
 
+        viewModel.moviesPopular.observe(viewLifecycleOwner, Observer {
+            var recyclerViewAdapterPopular: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), it)
+            recyclerViewPopular.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewPopular.adapter = recyclerViewAdapterPopular
+        })
 
         return root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (DataManager.moviesNow.size == 0) getMoviesNow()
-        if (DataManager.moviesPopular.size == 0) getMoviesPopular()
-        if (DataManager.moviesIncoming.size == 0) getMoviesIncoming()
-
-        moviesNow = DataManager.moviesNow
-        moviesPopular = DataManager.moviesPopular
-        moviesIncoming = DataManager.moviesIncoming
+    private fun loadMovies() {
+        viewModel.getMoviesNow()
+        viewModel.getMoviesPopular()
+        viewModel.getMoviesIncoming()
     }
-
-    private fun getMoviesIncoming() {
-        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
-        var call = moviesService.getMoviesIncoming()
-        var moviesIncoming = ArrayList<Movie> ()
-
-        call.enqueue(object : Callback<ApiResponse> {
-            override fun onResponse(
-                call: Call<ApiResponse>,
-                response: Response<ApiResponse>
-            ) {
-                var response = response.body()
-                for (i in response!!.results){
-                    moviesIncoming.add(fromResultToMovie(i))
-                }
-                DataManager.moviesIncoming = moviesIncoming
-                var recyclerViewAdapterIncoming: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesIncoming)
-                recyclerViewIncoming.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-                recyclerViewIncoming.adapter = recyclerViewAdapterIncoming
-            }
-
-            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                Toast.makeText(activity, t.message, Toast.LENGTH_SHORT).show()
-                Log.d("tag", t.message.toString())
-            }
-        })
-    }
-
-    private fun getMoviesPopular() {
-        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
-        var call = moviesService.getMoviesPopular()
-        var moviesPopular = ArrayList<Movie> ()
-
-        call.enqueue(object : Callback<ApiResponse> {
-            override fun onResponse(
-                call: Call<ApiResponse>,
-                response: Response<ApiResponse>
-            ) {
-                var response = response.body()
-                for (i in response!!.results){
-                    moviesPopular.add(fromResultToMovie(i))
-                }
-                DataManager.moviesPopular = moviesPopular
-                var recyclerViewAdapterPopular: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesPopular)
-                recyclerViewPopular.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-                recyclerViewPopular.adapter = recyclerViewAdapterPopular
-            }
-
-            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                Toast.makeText(activity, t.message, Toast.LENGTH_SHORT).show()
-                Log.d("tag", t.message.toString())
-            }
-        })
-    }
-
-    private fun getMoviesNow(){
-        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
-        var call = moviesService.getMoviesNow()
-        var moviesNow = ArrayList<Movie> ()
-
-        call.enqueue(object : Callback<ApiResponse> {
-            override fun onResponse(
-                call: Call<ApiResponse>,
-                response: Response<ApiResponse>
-            ) {
-                var response = response.body()
-                for (i in response!!.results){
-                    moviesNow.add(fromResultToMovie(i))
-                }
-                DataManager.moviesNow = moviesNow
-                var recyclerViewAdapterNow: RecyclerViewAdapter = RecyclerViewAdapter(requireContext(), moviesNow)
-                recyclerViewNow.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-                recyclerViewNow.adapter = recyclerViewAdapterNow
-            }
-
-            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                Toast.makeText(activity, t.message, Toast.LENGTH_SHORT).show()
-                Log.d("tag", t.message.toString())
-            }
-        })
-    }
-
 }
--- app/src/main/java/com/example/mymovieapp/ui/home/MoviesViewModel.kt
+++ app/src/main/java/com/example/mymovieapp/ui/home/MoviesViewModel.kt
@@ -1,13 +1,106 @@
 package com.example.mymovieapp.ui.home
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mymovieapp.RecyclerViewAdapter
+import com.example.mymovieapp.models.DataManager
+import com.example.mymovieapp.models.Movie
+import com.example.mymovieapp.models.fromResultToMovie
+import com.example.mymovieapp.modelsApi.ApiResponse
+import com.example.mymovieapp.services.MoviesApi
+import com.example.mymovieapp.services.ServiceBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MoviesViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _moviesNow: MutableLiveData<List<Movie>> = MutableLiveData()
+    private val _moviesPopular: MutableLiveData<List<Movie>> = MutableLiveData()
+    private val _moviesIncoming: MutableLiveData<List<Movie>> = MutableLiveData()
+    private val _status: MutableLiveData<Boolean> = MutableLiveData<Boolean>().apply {
+        value = false
     }
-    val text: LiveData<String> = _text
+
+    val moviesNow: LiveData<List<Movie>> = _moviesNow
+    val moviesPopular: LiveData<List<Movie>> = _moviesPopular
+    val moviesIncoming: LiveData<List<Movie>> = _moviesIncoming
+    val status: LiveData<Boolean> = _status
+
+    fun getMoviesNow(){
+        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
+        var call = moviesService.getMoviesNow()
+        var moviesNow: List<Movie> = emptyList()
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(
+                call: Call<ApiResponse>,
+                response: Response<ApiResponse>
+            ) {
+                var response = response.body()
+                for (i in response!!.results){
+                    moviesNow = moviesNow.plus(fromResultToMovie(i))
+                }
+                _moviesNow.value = moviesNow
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                _status.value = true
+                Log.d("tag", t.message.toString())
+            }
+        })
+    }
+
+    fun getMoviesIncoming() {
+        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
+        var call = moviesService.getMoviesIncoming()
+        var moviesIncoming: List<Movie> = emptyList()
+
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(
+                call: Call<ApiResponse>,
+                response: Response<ApiResponse>
+            ) {
+                var response = response.body()
+                for (i in response!!.results){
+                    moviesIncoming = moviesIncoming.plus(fromResultToMovie(i))
+                }
+                _moviesIncoming.value = moviesIncoming
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                _status.value = true
+                Log.d("tag", t.message.toString())
+            }
+        })
+    }
+
+    fun getMoviesPopular() {
+        var moviesService = ServiceBuilder.buildService(MoviesApi::class.java)
+        var call = moviesService.getMoviesPopular()
+        var moviesPopular: List<Movie> = emptyList()
+
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(
+                call: Call<ApiResponse>,
+                response: Response<ApiResponse>
+            ) {
+                var response = response.body()
+                for (i in response!!.results){
+                    moviesPopular = moviesPopular.plus(fromResultToMovie(i))
+                }
+                _moviesPopular.value = moviesPopular
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                _status.value = true
+                Log.d("tag", t.message.toString())
+            }
+        })
+    }
+
+
 }
